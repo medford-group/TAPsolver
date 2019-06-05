a2886942982fa4125b2bc58e9a08d5a3db533a80
Yonge, Adam Christopher <ayonge3@gatech.edu>
2019-06-05 15:05:21 -0400

    with some initial rate folders

--- shared_analysis/ross/to_ross/lang_folder/thin_data/thin_zone_visualization.py
+++ shared_analysis/ross/to_ross/lang_folder/thin_data/thin_zone_visualization.py
@@ -9,6 +9,8 @@ import os
 sim_info = pd.read_csv('../input_file.csv',header = None)
 curr_time_steps = 250
 
+
+
 length = float(sim_info[2][3])
 mesh_size = float(sim_info[2][4])
 time_steps = float(sim_info[2][2])
@@ -16,10 +18,13 @@ time_tot = float(sim_info[2][1])
 cat_frac = float(sim_info[2][5])
 reac_radius = float(sim_info[2][6])
 
-
-species = 'O*'
+species = 'CO*'
 
 thin_data = pd.read_csv('./'+species+'.csv',header = None)
+
+for k in range(0,thin_data.shape[0]-1):
+	thin_data.iloc[k] = thin_data.iloc[k+1] - thin_data.iloc[k]  
+
 #print(thin_data.iloc[2].sum()*)
 
 x_length = mp.ceil(cat_frac*mesh_size)
@@ -64,7 +69,7 @@ def tap_plot(step):
 	ax.grid()
 	props = dict(facecolor='white')
 
-	subpos = [0.4,0.35,0.5,0.5]
+	subpos = [0.4,0.15,0.5,0.35]
 	#subpos = [0.2,0.6,0.3,0.3]
 	subax1 = add_subplot_axes(ax,subpos)
 	subax1.plot(time,av_conc)
@@ -72,7 +77,8 @@ def tap_plot(step):
 	subax1.set_ylabel('$nmol/cm3$')
 	subax1.set_xlabel('$t\ (seconds)$')
 	subax1.set_xlim(0,0.15)
-	subax1.set_ylim(120000,139000)
+	subax1.set_ylim(-1,1)
+	subax1.hlines(y=0,xmin=0,xmax=0.15,linestyle='--',colors = 'k')
 
 	ax.scatter(x_dim, thin_data.iloc[step])
 	ax.set_xlabel('$Catalyst\ Zone\ (cm)$', fontsize=16)
@@ -83,8 +89,8 @@ def tap_plot(step):
 	ax.hlines(y=av_value,xmin=(length/mesh_size),xmax=(x_length-1)*(length/mesh_size),linestyle='--',colors = 'b')
 	#ax.set_ylim()
 	ax.set_xlim(0, x_length*(length/mesh_size))
-	#ax.set_ylim(0, 20)
-	ax.set_ylim(138750,139000)
+	ax.set_ylim(-1, 1)
+	#ax.set_ylim(270000, 280000)
 
 	fig.canvas.draw()       # draw the canvas, cache the renderer
 	image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
@@ -94,4 +100,4 @@ def tap_plot(step):
 
 kwargs_write = {'fps':20.0, 'quantizer':'nq'}
 
-imageio.mimsave('./'+species+'.gif', [tap_plot(i) for i in range(curr_time_steps)], fps=20)
+imageio.mimsave('./r_'+species+'.gif', [tap_plot(i) for i in range(curr_time_steps)], fps=20)
