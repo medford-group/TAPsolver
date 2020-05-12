135ccb9d0c611c1bc7191a7df4090f5e96badaac
ayonge3 <ayonge3@gatech.edu>
2020-05-12 18:59:32 -0400

    package formate for TAPsolver

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='tapsolver',
+      version='0.1',
+      description='A simulation and analysis tool for TAP reactor systems',
+      url='http://github.com/medford-group/TAPsolver',
+      author='Adam Yonge',
+      author_email='ayonge3@gatech.edu',
+      license='MIT',
+      packages=['tapsolver'],
+      install_requires=[
+      'fenics', 'dolfin-adjoint', 'imageio', 'pandas', 'xlrd'],
+      zip_safe=False)
