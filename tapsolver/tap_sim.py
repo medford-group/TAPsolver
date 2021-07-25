3e847bb5a9d06056d504ea03145c987f23d1240e
ayonge3 <ayonge3@gatech.edu>
2021-07-24 21:29:23 -0400

    Correct function based variation of initial states

--- tapsolver/tap_sim.py
+++ tapsolver/tap_sim.py
@@ -2166,14 +2166,17 @@ def fitting_gif(timeFunc,input_file = './input_file.csv',x_scale='',y_scale='',o
 
 def vary_Input(variable_type=None,variableToChange=None, newValue=0, input_file='./input_file.csv'):
 	df1 = pd.read_csv(input_file,header = None)
-	cellRow = df1[df1==variableToChange].index.values[0]
-	cellCol = df1[df1==variableToChange].index.values[1]
+	a = df1.where(df1==variableToChange).dropna(how='all').dropna(axis=1)
+	cellCol = a_vary_input.columns[0]
+	cellRow = a_vary_input.index[0]
 	
 	if variable_type == ('intensity' or 'surface') and variableToChange != None:
 		df1.iloc[cellRow+1,cellCol] = newValue
 	elif variable_type == 'delay' and variableToChange != None:
 		df1.iloc[cellRow+2,cellCol] = newValue
 	elif variable_type == 'Temperature':
+		df1.iloc[cellRow,1] = newValue
+	elif variableToChange == 'Output Folder Name':
 		df1.iloc[cellRow,1] = newValue
 	else:
 		print('Variable not found in input file.')
