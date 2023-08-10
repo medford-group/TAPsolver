fac6c62c18d6893ae2e03d8df9e28d55315da783
ayonge3 <ayonge@nrel.gov>
2023-08-10 17:03:33 -0600

    Step 1 Purging old files

--- tapsolver/define_adspecies.py
+++
@@ -1,8 +0,0 @@
-
-# Copyright 2021, Battelle Energy Alliance, LLC All Rights Reserved
-
-class define_adspecies():
-	def __init__(self):
-		self.concentration = 0
-		self.noise = 0 
-		self.sigma = 1
